1ebbae28e945fb8cb6dc4a007db9eb82a8eef56e
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-10-17 13:19:45 +0900

    iOSで周囲にあるペリフェラル全部キャッチする処理を追加

--- BeaconApp-iOS/BeaconApp/ViewController.swift
+++ BeaconApp-iOS/BeaconApp/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
+    var peripheral: CBPeripheral!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - CBCentralManagerDelegate
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        print("central state: \(central.state)")
+        switch central.state {
+        case .PoweredOn:
+            self.centralManager.scanForPeripheralsWithServices(nil, options: nil)
+        default:
+            return
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        print("peripheral: \(peripheral)")
+//        self.centralManager.stopScan()
+        
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        
+    }
+    
+    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        
+    }
+    
+    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        
+    }
+    
+    func centralManager(central: CBCentralManager, willRestoreState dict: [String : AnyObject]) {
+        
+    }
 }
 
